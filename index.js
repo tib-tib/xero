b642f67490f1f14e146c334dbf9469234a154c87
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2017-02-15 10:33:05 +0100

    Add hack to post multiple tracking data

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ Xero.prototype.call = function(method, path, body, callback) {
             content_type = 'application/xml';
         }
     }
+
+    if(post_body) {
+        // Hack for Tracking Categories
+        post_body = post_body.replace(/TrackingCategories/g, 'Tracking');
+    }
+
     var process = function(err, xml, res) {
         if (err) {
             return callback(err);
